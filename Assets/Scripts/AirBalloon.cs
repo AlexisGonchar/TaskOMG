935990b0285883686ba8237fccb16021219866e9
Gonchar Alexis <alexgonchar00@gmail.com>
2020-04-06 13:21:01 +0300

    Added the generation of air balloons and their movement.

---
+++ Assets/Scripts/AirBalloon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Класс для описания поведения воздушного шара на сцене.
+public class AirBalloon : MonoBehaviour
+{
+	//Направление движения воздушного шара.
+	private int direction;
+	//Скорость движения воздушного шара.
+	private float speed;
+	//Начальная позиция воздушного шара.
+	private Vector3 pos;
+
+	void Start()
+	{
+		//Задание направления движение воздушного шара.
+		direction = transform.position.x < 0 ? 1 : -1;
+		pos = transform.position;
+		//Генерация случайной скорости.
+		speed = Random.Range(0.5f, 1.5f);
+	}
+
+	void Update()
+	{
+		float edge = AirBalloonGenerate.screenEdge.x;
+		//Уничтожение воздушного шара после прохождения сцены.
+		if (pos.x > edge || pos.x < -edge)
+		{
+			Destroy(this.gameObject);
+			AirBalloonGenerate.balloonCount--;
+		}
+		else
+		{
+			//Перемещение по горизонтали.
+			pos += transform.right * Time.deltaTime * speed * direction;
+			//Движение по синусоиде.
+			transform.position = pos + transform.up * Mathf.Sin(Time.time * 2) * 0.5f;
+		}
+	}
+}
---
+++ Assets/Scripts/AirBalloonGenerate.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Класс для генерации воздушных шаров на сцене.
+public class AirBalloonGenerate : MonoBehaviour
+{
+	//Игровые объекты воздушных шаров.
+	public GameObject AirBalloon1;
+	public GameObject AirBalloon2;
+	//Количество воздушных шаров на сцене.
+	public static int balloonCount;
+	//Правый верхний край экрана.
+	public static Vector2 screenEdge;
+
+	void Start()
+	{
+		//Вычисление края экрана.
+		screenEdge = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+		screenEdge.x += AirBalloon1.GetComponent<BoxCollider2D>().size.x / 2f;
+		screenEdge.y -= AirBalloon1.GetComponent<BoxCollider2D>().size.y;
+		//Инициализация.
+		balloonCount = 0;
+	}
+
+	void Update()
+	{
+		//Генерация нового воздушного шара, если таковых меньше трёх на сцене.
+		if (balloonCount < 3)
+		{
+			GenerateAirBalloon();
+			balloonCount++;
+		}
+	}
+
+	//Метод генерации воздушных шаров.
+	void GenerateAirBalloon()
+	{
+		GameObject obj;
+		//Определение вида воздушного гара случайным образом.
+		obj = Random.Range(0, 2) > 0 ? AirBalloon1 : AirBalloon2;
+		//Определение стороны генерации воздушного шара на сцене случайным образом.
+		float x = Random.Range(0, 2) > 0 ? screenEdge.x : -screenEdge.x;
+		//Определение высоты генерации воздушного шара случайным образом.
+		float y = Random.Range(screenEdge.y , (-screenEdge.y + screenEdge.y * 0.5f));
+		//Создание объекта.
+		Instantiate(obj, new Vector2(x, y), Quaternion.identity);
+	}
+}
